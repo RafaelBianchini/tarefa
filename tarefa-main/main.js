1efcdd3cf29d53d149b6ead75174f283e1bc83f8
RafaelBianchini <rafael.adolfo.bianchini@escola.pr.gov.br>
2026-06-18 08:05:36 -0300

    Update main.js

--- tarefa-main/main.js
+++ tarefa-main/main.js
@@ -1,2 +1,21 @@
 const botoes = document.querySelectorAll("botao");
-    console.log(botoes);
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for(let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            botoes[j].classList.remove("ativo);
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
