6343d410805527c4b5e7ae44e470b12954c74b50
RafaelBianchini <rafael.adolfo.bianchini@escola.pr.gov.br>
2026-06-30 08:19:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,10 +17,10 @@
     const contadores = document.querySelectorAll(".contador");
 
 
-const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-25T00:00:00");
-const tempoObjetivo3 = new Date("2025-01-10T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-15T00:00:00");
+const tempoObjetivo1 = new Date("2026-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-25T00:00:00");
+const tempoObjetivo3 = new Date("2027-01-10T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-15T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
